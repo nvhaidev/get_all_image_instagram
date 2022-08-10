ae0678b24fd646bec55d1e689a3d277e7b9b1832
Nguyễn Văn Hải <91068849+nvhaidev@users.noreply.github.com>
2022-08-10 08:25:28 +0700

    update img class _aagt

--- main.js
+++ main.js
@@ -25,63 +25,65 @@ const fs = require("fs");
 
 //scan 
 (() => {
-  const data = [];
-  function clickNext() {
-    document
-      .querySelector(
-        "body > div.RnEpo._Yhr4 > div.Z2Inc._7c9RR > div > div.l8mY4.feth3 > button"
-      )
-      .click();
-    getImage();
-    setTimeout(clickNext, 3000);
-    console.log(data);
-  }
-  function getImage() {
-    const image = [];
-    document.querySelectorAll("img.FFVAD").forEach((value, keys) => {
-      image.push(
-        document.querySelectorAll("img.FFVAD")[keys].srcset.split(",")[
-          document.querySelectorAll("img.FFVAD")[keys].srcset.split(",")
-            .length - 1
-        ]
-      );
-    });
-    data.push(image);
-  }
+    const data = [];
+    function clickNext() {
+        document
+            .querySelector(
+                "#mount_0_0_wM > div > div > div > div:nth-child(4) > div > div > div.rq0escxv.l9j0dhe7.du4w35lb > div > div.iqfcb0g7.tojvnm2t.a6sixzi8.k5wvi7nf.q3lfd5jv.pk4s997a.bipmatt0.cebpdrjk.qowsmv63.owwhemhu.dp1hu0rb.dhp61c6y.l9j0dhe7.iyyx5f41.a8s20v7p > div > div > div > div > div._a3gq._ab-1 > div > div > div._aaqg._aaqh > button"
+            )
+            .click();
 
-  clickNext();
+        getImage();
+        setTimeout(clickNext, 3000);
+        console.log(data);
+    }
+    function getImage() {
+        const image = [];
+        document.querySelectorAll("img._aagt").forEach((value, keys) => {
+            image.push(
+                document.querySelectorAll("img._aagt")[keys].srcset.split(",")[
+                    document
+                        .querySelectorAll("img._aagt")
+                        [keys].srcset.split(",").length - 1
+                ]
+            );
+        });
+        data.push(image);
+    }
+
+    clickNext();
 })();
-
-
-//scan
 
 (() => {
-  var video = 0;
-  const data = [];
-  function getImage() {
-    document.querySelectorAll("img.FFVAD").forEach((value, keys) => {
-      data.push(
-        document.querySelectorAll("img.FFVAD")[keys].srcset.split(",")[
-          document.querySelectorAll("img.FFVAD")[keys].srcset.split(",")
-            .length - 1
-        ]
-      );
-    });
-  }
-
-  function pageScroll() {
-    getImage();
-    window.scrollBy(0, 1000);
-    video += 1;
-    let max = document.querySelectorAll("img.FFVAD").length;
-    if (video > max) {
-      console.log(data);
-      return true;
+    var video = 0;
+    const data = [];
+    function getImage() {
+        document.querySelectorAll("img._aagt").forEach((value, keys) => {
+            data.push(
+                document.querySelectorAll("img._aagt")[keys].srcset.split(",")[
+                    document
+                        .querySelectorAll("img._aagt")
+                        [keys].srcset.split(",").length - 1
+                ]
+            );
+        });
     }
-    setTimeout(pageScroll, 3000);
-  }
-  pageScroll();
+
+    function pageScroll() {
+        getImage();
+        window.scrollBy(0, 1000);
+        video += 1;
+        let max = document.querySelectorAll("img._aagt").length;
+        if (video > max) {
+            console.log(data);
+            return true;
+        }
+        setTimeout(pageScroll, 3000);
+    }
+    pageScroll();
 })();
+
+
 (async () => {
   const data = [];
   const uniqueChars = [...new Set(jsonData)];
