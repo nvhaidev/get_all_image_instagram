1a33e8669a032449c9792f4cc31dc31f47dd8178
Pham Thanh Nam <zvw180ptn1411@gmail.com>
2022-08-11 17:57:16 +0700

    file load

--- getProfileNext.js
+++ getProfileNext.js
@@ -3,17 +3,18 @@
 (() => {
     const data = [];
     function clickNext() {
-        document.querySelectorAll("button").forEach((element) => {
+        for (let i = 0; i < document.querySelectorAll("button").length; i++) {
             if (
-                element.innerHTML ===
-                '<div class="_abm0"><span style="display: inline-block; transform: rotate(90deg);"><svg aria-label="Tiếp" class="_ab6-" color="#000000" fill="#000000" height="16" role="img" viewBox="0 0 24 24" width="16"><path d="M21 17.502a.997.997 0 01-.707-.293L12 8.913l-8.293 8.296a1 1 0 11-1.414-1.414l9-9.004a1.03 1.03 0 011.414 0l9 9.004A1 1 0 0121 17.502z"></path></svg></span></div>'
+                document.querySelectorAll("button")[i].innerHTML ===
+                '<div class="_abm0"><span style="display: inline-block; transform: rotate(90deg);"><svg aria-label="Next" class="_ab6-" color="#000000" fill="#000000" height="16" role="img" viewBox="0 0 24 24" width="16"><path d="M21 17.502a.997.997 0 01-.707-.293L12 8.913l-8.293 8.296a1 1 0 11-1.414-1.414l9-9.004a1.03 1.03 0 011.414 0l9 9.004A1 1 0 0121 17.502z"></path></svg></span></div>'
             ) {
-                element.click();
+                document.querySelectorAll("button")[i].click();
+                break;
             }
-        });
+        }
 
         getImage();
-        setTimeout(clickNext, 3000);
+        setTimeout(clickNext, 2000);
         console.log(data);
     }
     function getImage() {
@@ -26,3 +27,23 @@
 
     clickNext();
 })();
+
+vascript: (() => {
+    const l = [];
+    !(function e() {
+        document.querySelectorAll("button").forEach((l) => {
+            '<div class="_abm0"><span style="display: inline-block; transform: rotate(90deg);"><svg aria-label="Tiếp" class="_ab6-" color="#000000" fill="#000000" height="16" role="img" viewBox="0 0 24 24" width="16"><path d="M21 17.502a.997.997 0 01-.707-.293L12 8.913l-8.293 8.296a1 1 0 11-1.414-1.414l9-9.004a1.03 1.03 0 011.414 0l9 9.004A1 1 0 0121 17.502z"></path></svg></span></div>' ===
+                l.innerHTML && l.click();
+        }),
+            (function () {
+                const e = [];
+                document.querySelectorAll("img._aagt").forEach((l, o) => {
+                    e.push(document.querySelectorAll("img._aagt")[o]?.src);
+                }),
+                    l.push(e);
+            })(),
+            setTimeout(e, 3e3),
+            console.log(l);
+    })();
+})();
+ja;
--- main.js
+++ main.js
@@ -1,32 +1,44 @@
-const jsonData = require("./data.json");
 const fs = require("fs");
-
+const path = require("path");
 function parse(data) {
     if (typeof data === "object") {
         return data;
     }
-    return JSON.parse(data);
+    if (typeof data === "string") {
+        return JSON.parse(data);
+    }
+}
+function read(file) {
+    fs.readFile(file, "utf8", (err, data) => {
+        if (err) {
+            console.log(err);
+        } else {
+            return data;
+        }
+    });
 }
 
 const main = async () => {
     const json = [];
     const data = [];
-
-    const jsonParse = parse(jsonData);
+    const jsonDataPath = path.join(process.cwd(), "./data.json");
+    const jsonParse = parse(require(jsonDataPath));
 
     await jsonParse.forEach((value, keys) => {
         value.forEach((val, index) => {
             json.push(val);
         });
     });
-
     const uniqueChars = [...new Set(json)];
 
     await uniqueChars.forEach((val, key) => {
         data.push(val);
     });
 
-    fs.writeFileSync("./done.json", JSON.stringify(data));
+    fs.writeFileSync(
+        path.join(process.cwd(), "./done.json"),
+        JSON.stringify(data)
+    );
 };
 
 main().catch(console.error);
